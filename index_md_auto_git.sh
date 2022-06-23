ec108be67b8a9e28b77a1f228d12b31b3f6c11f9
RogerJTX <fengdejiezou@126.com>
2022-06-23 17:19:17 +0800

    modified 2022/06/23 -17:19:17

--- index_md_auto_git.sh
+++ index_md_auto_git.sh
@@ -12,7 +12,7 @@ echo ${current_time}							# 显示当前时间
 
 git add .
 git commit -m "modified ${current_time}" 		# 远程仓库可以看到是什么时间修改的...
-git push origin main
+git push
 
 echo "####### 自动Git完成 #######"
 sleep 20s
